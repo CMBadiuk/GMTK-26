d0aa2530815a419e2c1a609892906dc422360a77
Caleb B <47331587+CMBadiuk@users.noreply.github.com>
2026-07-22 12:02:39 -0600

    Initial Project Commit, Game State and Timer

---
+++ global/game_state.gd
@@ -0,0 +1,32 @@
+## Game State - where all the core functions live, where the countdown goes
+extends Node
+
+signal time_changed(new_time)
+signal time_added(amount)
+signal game_over(reason)
+# signal game_won() # Keeping just in case, but I might want to do branches for different game over conditions instead
+
+var time_remaining := 300.0 # Time in seconds before Death takes you
+var is_running := true
+var win_threshold := 3000.0 # Time in seconds required to get Death off your back. Needs to be tuned
+var money := 0
+var stats := { "charisma": 1, "wit": 1, "sincerity": 1, "regret": 20}
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not is_running:
+		return
+	time_remaining -= delta
+	time_changed.emit(time_remaining)
+	if time_remaining <= 0.0:
+		is_running = false
+		game_over.emit("timeout")
+		# In whatever runs after this, add branches for different exposition based on the player's regret stat
+	elif time_remaining >= win_threshold:
+		is_running = false
+		game_over.emit("time_won")
+		
+func add_time(amount):
+	time_remaining += amount
+	time_added.emit(amount) # The UI plays a sweet, satisfying effect
+	time_changed.emit(time_remaining)
